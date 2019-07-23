38cff072e2872ea9bdf68a45f3ffd987f006eaa8
Richard Arima <richard.arima@gmail.com>
2019-07-23 17:03:26 -0400

    Initial Commit

---
+++ artnetConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  artnetConverter
+//
+//  Created by Richard Arima on 7/23/19.
+//  Copyright © 2019 Richard Arima. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
---
+++ artnetConverter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  artnetConverter
+//
+//  Created by Richard Arima on 7/23/19.
+//  Copyright © 2019 Richard Arima. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    // Do any additional setup after loading the view.
+}
+
+
+- (void)setRepresentedObject:(id)representedObject {
+    [super setRepresentedObject:representedObject];
+
+    // Update the view, if already loaded.
+}
+
+
+@end
